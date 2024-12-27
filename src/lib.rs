f85b3958049837bf95796843eeb35ac0e8044a9f
DarkSky <darksky2048@gmail.com>
2024-12-28 03:23:14 +0800

    feat: improve register

--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,38 @@ use vfs::HttpVfs;
 
 pub use vfs::HTTP_VFS;
 
-pub fn register_http_vfs() {
-    const ONCE: Once = Once::new();
+pub struct HttpVfsRegister {
+    block_size: usize,
+    download_threshold: usize,
+}
 
-    ONCE.call_once(|| {
-        let _ = register(HTTP_VFS, HttpVfs::default(), true);
-    })
+impl HttpVfsRegister {
+    pub fn new() -> Self {
+        Self {
+            block_size: 1024 * 1024 * 4,
+            download_threshold: 1024,
+        }
+    }
+
+    pub fn with_block_size(self, block_size: usize) -> Self {
+        Self { block_size, ..self }
+    }
+
+    pub fn register(self) {
+        const ONCE: Once = Once::new();
+
+        let vfs_instance = HttpVfs {
+            block_size: self.block_size,
+            download_threshold: self.download_threshold,
+        };
+
+        ONCE.call_once(|| {
+            let _ = register(HTTP_VFS, vfs_instance, true);
+        })
+    }
+}
+
+#[inline(always)]
+pub fn register_http_vfs() {
+    HttpVfsRegister::new().register();
 }
